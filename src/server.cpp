f6053f626a5c4e132fe6403f922a14c3143a1e74
Peter Trinh <petertrinh04@gmail.com>
2026-06-21 23:06:33 -0400

    Add bind(), listen(), accept(), recv(), and send()

--- src/server.cpp
+++ src/server.cpp
@@ -11,7 +11,7 @@ Server::Server(int port) : m_port{port} {}
 
 void Server::start()
 {
-    std::cout << "Starting server on port " << m_port << '\n';
+    std::cout << "Starting server on port " << m_port << '\n'; // socket()
 
     int server_fd{socket(AF_INET, SOCK_STREAM, 0)};
 
@@ -22,6 +22,78 @@ void Server::start()
     }
 
     std::cout << "Socket created successfully\n";
+
+    sockaddr_in server_address{}; // bind()
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(m_port);
+
+    if (bind(server_fd, reinterpret_cast<sockaddr*>(&server_address),
+             sizeof(server_address)) == -1)
+    {
+        std::perror("bind");
+        close(server_fd);
+        return;
+    }
+
+    std::cout << "Socket bound to port " << m_port << '\n';
+
+    if (listen(server_fd, SOMAXCONN) == -1) // listen()
+    {
+        std::perror("listen");
+        close(server_fd);
+        return;
+    }
+
+    std::cout << "Listening for connections on port " << m_port << '\n';
+
+    std::cout << "Waiting for a client...\n";
+
+    sockaddr_in client_address{};
+    socklen_t client_length{sizeof(client_address)};
+
+    int client_fd{accept(server_fd,
+                         reinterpret_cast<sockaddr*>(&client_address),
+                         &client_length)};
+
+    if (client_fd == -1)
+    {
+        std::perror("accept");
+        close(server_fd);
+        return;
+    }
+
+    std::cout << "Client connected!\n";
+
+    char buffer[4096]{}; // recv()
+
+    ssize_t bytes_received{recv(client_fd, buffer, sizeof(buffer) - 1, 0)};
+
+    if (bytes_received < 0)
+    {
+        std::perror("recv");
+        close(client_fd);
+        close(server_fd);
+        return;
+    }
+
+    buffer[bytes_received] = '\0';
+
+    std::cout << "Received request:\n"; // send()
+    std::cout << buffer << '\n';
+
+    const char* response{"HTTP/1.1 200 OK\r\n"
+                         "Content-Type: text/plain\r\n"
+                         "\r\n"
+                         "Hello World\n"};
+
+    ssize_t bytes_sent{send(client_fd, response, std::strlen(response), 0)};
+
+    if (bytes_sent < 0)
+    {
+        std::perror("send");
+    }
 
     close(server_fd);
 }
